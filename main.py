9c5ba63f77832c450e6f64654226587c42735d68
Uday Prakash Gupta <70849646+CodingWithUday@users.noreply.github.com>
2021-02-14 14:32:39 +0530

    Updated Main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from moviepy.editor import VideoFileClip #pip install moviepy
 
-clip = VideoFileClip("vid.mp4")
+clip = VideoFileClip("vid.mp4") #Adding The Video File
 
-clip.write_gif("myd.gif", fps=6)
+clip.write_gif("myd.gif", fps=6) #Naming The Gif And Setting Up The FPS Value 
